6e57e1b23314c61acc615b6c4bec7a45b4544f6b
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-09-14 19:58:21 +0300

    Update: tailwind.config.js
    Replace deafult tailwind utility separator from colon to underscore
    Because pug doesn't support this symbol to use in classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{pug,scss}"],
+
   theme: {
     extend: {
       //
     },
   },
+
+  separator: '_',
+
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
